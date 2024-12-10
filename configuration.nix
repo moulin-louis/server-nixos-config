357d3ccd99af4b2ef1355d78fe78c339f55b9751
moulin-louis <louis.moulin@outlook.fr>
2024-12-10 22:31:15 +0100

    update mouse accel options, add nixos cachix to trusted keys

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
     ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     experimental-features = [
       "nix-command"
       "flakes"
@@ -56,7 +56,9 @@
       variant = "";
     };
     videoDrivers = [ "nvidia" ];
-    libinput.mouse.accelProfile = "flat";
+  };
+  services.libinput.mouse = {
+    accelProfile = "flat";
   };
 
   # Sound
