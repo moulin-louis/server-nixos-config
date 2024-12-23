4b7af282b3e6f576bb4f8b3ee89ce2428748e3b9
moulin-louis <louis.moulin@outlook.fr>
2024-12-23 19:06:37 +0100

    add back jellyfin services and change hw accel to match nixos jellyfin docs

--- configuration.nix
+++ configuration.nix
@@ -58,12 +58,20 @@
     shell = pkgs.fish;
   };
 
+  # 1. enable vaapi on OS-level
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-sdk
       intel-media-driver
-      intel-vaapi-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
     ];
   };
 
@@ -97,6 +105,12 @@
     };
   };
   networking.firewall.allowedTCPPorts = [ 22 ];
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "llr";
+  };
 
 
   system.stateVersion = "24.11";
